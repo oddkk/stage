531b08cae780341d8c67766b9d0a4102e0de0b2d
Odd Kristian Kvarmestøl <okknor@gmail.com>
2020-04-16 15:37:19 +0200

    Implement basic sql query

--- modules/sql/src/mod.c
+++ modules/sql/src/mod.c
@@ -3,6 +3,7 @@
 #include <base/mod.h>
 #include <dlist.h>
 #include <stdlib.h>
+#include <base/list.h>
 
 #include <libpq-fe.h>
 
@@ -57,19 +58,24 @@ sql_db_connect_unsafe(struct vm *vm, struct stg_exec *heap,
 	memcpy(out, &connection_id, sizeof(int64_t));
 }
 
+void
+sql_db_connect_copy(struct stg_exec *heap, void *data)
+{
+	struct sql_connect_data *closure = data;
+	closure->con_str = stg_exec_copy_string(heap, closure->con_str);
+}
+
 struct stg_init_data
 sql_db_connect(struct stg_exec *ctx, struct string kind, struct string con_str)
 {
 	struct stg_init_data monad = {0};
 	monad.call = sql_db_connect_unsafe;
-	// TODO: Copy strings.
-	// monad.copy = ...;
+	monad.copy = sql_db_connect_copy;
 	monad.data_size = sizeof(struct sql_connect_data);
 	monad.data = stg_alloc(ctx, 1, monad.data_size);
 
 	struct sql_connect_data *closure;
 	closure = monad.data;
-	// TODO: Copy strings.
 	closure->kind = kind;
 	closure->con_str = con_str;
 
@@ -96,25 +102,101 @@ sql_db_query_unsafe(struct vm *vm, struct stg_exec *heap,
 	struct sql_connection *db;
 	db = &ctx->connections[closure->db];
 
-	printf("Query db %zi '%.*s'\n",
-			closure->db, LIT(closure->query));
+	struct arena *tmp_mem = &vm->transient;
+	arena_mark cp = arena_checkpoint(tmp_mem);
+
+	// To Zero-terminated string
+	char *query;
+	query = arena_alloc(tmp_mem, closure->query.length + 1);
+	memcpy(query, closure->query.text, closure->query.length);
+	query[closure->query.length] = '\0';
+
+	PGresult *res;
+	res = PQexec(db->con, query);
+
+	arena_reset(tmp_mem, cp);
+
+	ExecStatusType status;
+	status = PQresultStatus(res);
+
+	struct stg_list_data *out_list = out;
+	memset(out_list, 0, sizeof(struct stg_list_data));
+
+	type_id out_type = stg_list_register_type(
+			sql_mod, vm->default_types.string);
+
+	switch (status) {
+		case PGRES_EMPTY_QUERY:
+		case PGRES_COMMAND_OK:
+			*out_list = stg_list_empty(
+					vm, heap, out_type);
+			break;
+
+		case PGRES_TUPLES_OK:
+			{
+				int num_rows, num_cols;
+				num_rows = PQntuples(res);
+				num_cols = PQnfields(res);
+
+				struct string *result;
+				result = stg_alloc(heap, num_rows * num_cols, sizeof(struct string));
+
+				for (int r = 0; r < num_rows; r++) {
+					for (int c = 0; c < num_cols; c++) {
+						struct string *field;
+						field = &result[r*num_cols + c];
+
+						field->length = PQgetlength(res, r, c);
+						field->text = PQgetvalue(res, r, c);
+						*field = stg_exec_copy_string(heap, *field);
+					}
+				}
+
+				struct stg_list_data *rows;
+				rows = stg_alloc(heap, num_rows, sizeof(struct stg_list_data));
+
+				for (int r = 0; r < num_rows; r++) {
+					rows[r] = stg_list_from_carray(
+							vm, heap, vm->default_types.string,
+							&result[r*num_cols], num_cols);
+				}
+
+				*out_list = stg_list_from_carray(
+						vm, heap, out_type, rows, num_rows);
+			}
+			break;
+
+		case PGRES_BAD_RESPONSE:
+		case PGRES_NONFATAL_ERROR:
+		case PGRES_FATAL_ERROR:
+		default:
+			PQresultErrorMessage(res);
+			panic("Query failed.");
+			break;
+	}
+
+	PQclear(res);
+}
+
+void
+sql_db_query_copy(struct stg_exec *heap, void *data)
+{
+	struct sql_query_data *closure = data;
+	closure->query = stg_exec_copy_string(heap, closure->query);
 }
 
 struct stg_init_data
 sql_db_query(struct stg_exec *ctx, int64_t db,
-		struct string query, type_id param_type,
-		type_id result_type)
+		struct string query)
 {
 	struct stg_init_data monad = {0};
 	monad.call = sql_db_query_unsafe;
-	// TODO: Copy strings.
-	// monad.copy = ...;
+	monad.copy = sql_db_query_copy;
 	monad.data_size = sizeof(struct sql_query_data);
 	monad.data = stg_alloc(ctx, 1, monad.data_size);
 
 	struct sql_query_data *closure;
 	closure = monad.data;
-	// TODO: Copy strings.
 	closure->db = db;
 	closure->query = query;
 
